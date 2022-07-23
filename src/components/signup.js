a6b5966b095e6b26cb236bcd218e1f56d8e414b7
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-07-23 05:45:14 +0200

    test pul

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,4 @@
-import {useHistory} from 'react-router-dom'
+// import {useHistory} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
 function signUp(){
@@ -8,17 +8,17 @@ function signUp(){
         height: '30px',
         marginTop: '3%',
     }
-    let history = useHistory()
-    const Register = (()=>{
-        history.push('/home')
-    })
+    // let history = useHistory()
+    // const Register = (()=>{
+    //     history.push('/home')
+    // })
     return(
         <div className="Container">
             <h1>Register account here</h1>
             <input type="email" placeholder="Enter your email" /><br/><br/>
             <input type="password" placeholder="Enter your password" />
-
-            <button style={btn} onClick={Register}>Sign Up</button>
+{/* 
+            <button style={btn} onClick={Register}>Sign Up</button> */}
 
             <span>Already have an account</span> <span>
                 <Link to='/Login'> Login</Link>
